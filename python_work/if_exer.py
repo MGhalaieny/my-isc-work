15088df80ddb9de46a2a0f29cc86b98b423ccf94
MGhalaieny <mohdgh@gmail.com>
2017-11-14 16:44:46 +0000

    If exercise

---
+++ python_work/if_exer.py
@@ -0,0 +1,11 @@
+#Try out if and elif and else
+
+name = 'Bart'
+
+if name == 'Lisa':
+    print name,'Plays the saxophone'
+elif name == 'Bart':
+    print name,'rides a skateboard'
+else:
+    print name, 'lives in Springfield'
+
